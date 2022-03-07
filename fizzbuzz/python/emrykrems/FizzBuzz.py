fedac4beec5613a20865cf90e25718351fa3d8ca
Emry <emry.krems@yahoo.com>
2022-03-07 16:23:03 -0700

    add comments to FizzBuzz.py

--- fizzbuzz/python/emrykrems/FizzBuzz.py
+++ fizzbuzz/python/emrykrems/FizzBuzz.py
@@ -1,5 +1,10 @@
 import sys
 
+# FizzBuzz function receives an integer from command line input
+# and tests from 1 to that integer if the number is divisible by
+# 3 or 5. If divisible by 3, print "Fizz", if divisible by 5, print
+# "Buzz", if divisible by 3 and 5, print "FizzBuzz".
+
 def FizzBuzz(input_number):
     line_to_print = ""
     for num in range(1, input_number+1):
@@ -13,4 +18,10 @@ def FizzBuzz(input_number):
             print(line_to_print)
         line_to_print = ""
 
-FizzBuzz(int(sys.argv[1]))
+# main funtion receives integer entered from command line
+# and calls FizzBuzz() function with that integer as an argument
+
+def main(input_number):
+    FizzBuzz(input_number)
+
+main(int(sys.argv[1]))
