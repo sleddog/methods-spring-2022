bf16dc400585fde02a9cbcce7bc7813acf67a50e
bryndonw <bryndonw99@gmail.com>
2022-03-29 11:50:18 -0600

    fixing formatting, adding comments, adding fizzbuzz readme to student page

--- fizzbuzz/fortran/brynwilkerson/fizzbuzz.f90
+++ fizzbuzz/fortran/brynwilkerson/fizzbuzz.f90
@@ -2,12 +2,14 @@
 !Bryn Wilkerson
 
 Program FizzBuzz
-    character(100) :: num
+    character(8) :: num, i_char
     integer :: n 
-
+        
+    ! reads in and stores the number to character
     call get_command_argument(1,num)
+    ! converts character to integer
     read(num,*)n
-
+        
     do i = 1, n
         if (mod(i,3) == 0) then
              if (mod(i,5) == 0) then
@@ -18,7 +20,10 @@ Program FizzBuzz
         else if (mod(i,5) == 0) then
             print *, "Buzz "
         else
-            print *, i
+            ! if the integer is not divisible by 3 and/or 5,
+            ! convert to character for formatting purposes
+            write(i_char, '(i8)') i
+            print *, adjustl(i_char)
         end if
     end do
 End Program FizzBuzz
