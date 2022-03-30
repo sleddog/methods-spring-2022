aa6586c028c1151a72c6ce8c1067f537aa079db6
cdanielson22 <b10f741@montana.edu>
2022-03-29 19:12:52 -0600

    making unit tests for turner

--- fizzbuzz/python/turnerburchard/fizzbuzz.py
+++ fizzbuzz/python/turnerburchard/fizzbuzz.py
@@ -1,14 +1,65 @@
-import sys
+import unittest
 
-def fizzbuzz(count):
+def fizzbuzz(n):
     stringOut = ""
-
-    for i in range(1, count):
-        if i%3 == 0: stringOut += "Fizz"
-        if i%5 == 0: stringOut += "Buzz"
-        if (i%3 != 0 and i%5 != 0): stringOut += str(i)
-        stringOut += "\n"
-
+    if n%3 == 0:
+        stringOut += "Fizz"
+    if n%5 == 0: 
+        stringOut += "Buzz"
+    if n%7 == 0:
+        stringOut += "Ping"
+    if n%11 == 0:
+        stringOut += "Pong"
+    if (n%3 != 0 and n%5 != 0 and n%7 != 0 and n%11 != 0): 
+        stringOut += str(n)
     return stringOut
-    
-print(fizzbuzz(int(sys.argv[1]) + 1))
+
+class TestFizzBuzz(unittest.TestCase):
+    def testFizz(self):
+        print("Testing Fizz ...")
+        if (self.assertEqual("Fizz", fizzbuzz(3)) == None):
+            print("Test Passed")
+        else:
+            print("Test Failed")
+
+    def testBuzz(self):
+        print("Testing Buzz ...")
+        if (self.assertEqual("Buzz", fizzbuzz(5)) == None):
+            print("Test Passed")
+        else:
+            print("Test Failed")
+
+    def testFizzBuzz(self):
+        print("Testing FizzBuzz ...")
+        if (self.assertEqual("FizzBuzz", fizzbuzz(15)) == None):
+            print("Test Passed")
+        else:
+            print("Test Failed")
+
+    def test2(self):
+        print("Testing 2 ...")
+        if (self.assertEqual("2", fizzbuzz(2)) == None):
+            print("Test Passed")
+        else:
+            print("Test Failed")
+
+    def testPing(self):
+        print("Testing Ping ...")
+        if (self.assertEqual("Ping", fizzbuzz(7)) == None):
+            print("Test Passed")
+        else:
+            print("Test Failed")
+
+    def testPong(self):
+        print("Testing Pong ...")
+        if (self.assertEqual("Pong", fizzbuzz(11)) == None):
+            print("Test Passed")
+        else:
+            print("Test Failed")
+
+    def testFizzBuzzPingPong(self):
+        print("Testing FizzBuzzPingPong ...")
+        if (self.assertEqual("FizzBuzzPingPong", fizzbuzz(1155)) == None):
+            print("Test Passed")
+        else:
+            print("Test Failed")
