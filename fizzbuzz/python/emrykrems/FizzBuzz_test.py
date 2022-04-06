edc039b28de8040a08d43eec9a9a0450a25d32c2
brycecoder37 <73134123+brycecoder37@users.noreply.github.com>
2022-04-05 20:10:46 -0600

    added more unit tests

--- fizzbuzz/python/emrykrems/FizzBuzz_test.py
+++ fizzbuzz/python/emrykrems/FizzBuzz_test.py
@@ -1,5 +1,11 @@
 from FizzBuzz import FizzBuzz
 
+def test_2_For2():
+    assert FizzBuzz(2) == 2
+
+def test_9_For13():
+    assert FizzBuzz(13) == 13
+
 def test_Fizz_For3():
     assert FizzBuzz(3) == "Fizz"
 
@@ -14,6 +20,21 @@ def test_Pong_For11():
 
 def test_FizzBuzz_For15():
     assert FizzBuzz(15) == "FizzBuzz"
+
+def test_FizzPing_For21():
+    assert FizzBuzz(21) == "FizzPing"
+
+def test_FizzPong_For33():
+    assert FizzBuzz(33) == "FizzPong"
+
+def test_BuzzPing_For35():
+    assert FizzBuzz(35) == "BuzzPing"
+
+def test_BuzzPong_For55():
+    assert FizzBuzz(55) == "BuzzPong"
+
+def test_BuzzPingPong_For385():
+    assert FizzBuzz(385) == "BuzzPingPong"
 
 def test_FizzBuzzPing_For105():
     assert FizzBuzz(105) == "FizzBuzzPing"
