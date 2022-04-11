4567acb6d0efc4326859445080a0d1446faa5c31
Logan Dolechek <logand.msu@gmail.com>
2022-04-11 17:44:00 -0600

    modified Chris fizz.py & added test cases

--- fizzbuzz/python/ChrisD/fizz.py
+++ fizzbuzz/python/ChrisD/fizz.py
@@ -1,4 +1,6 @@
+import unittest
 import sys
+
 
 def fizz(num):
     for i in range(num+1):
@@ -6,8 +8,12 @@ def fizz(num):
             print("fizz", end = "")
         if (i % 5 == 0):
             print("buzz", end = "")
-        if(i % 3 > 0 and i % 5 > 0):
+        if(i % 3 > 0 and i % 5 > 0 and i % 7 > 0 and i % 11 > 0):
             print(i, end = "")
+        if (i % 7 == 0):
+            print("ping", end = "")
+        if (i % 11 == 0):
+            print("pong", end = "")
         print()
 
 fizz(int(sys.argv[1]))
