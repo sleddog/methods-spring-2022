b349e970e4d779e802886f27b303c4ba495c32ec
cweeding1 <cweeding1@gmail>
2022-04-11 17:16:58 -0600

    added Ping/Pong and test cases

---
+++ fizzbuzz/python/Phred7/testFizzBuzz.py
@@ -0,0 +1,28 @@
+import unittest
+import fizz_buzz
+
+if __name__ == "__main__":
+    str = fizz_buzz.fizz_buzz(3).split()[-1]
+    print(str)
+
+class TestFizzBuzz(unittest.TestCase):
+    def test_fizz(self):
+        print("Testing Fizz ...")
+        self.assertEqual("fizz", fizz_buzz.fizz_buzz(3).split()[-1].lower())
+
+    def test_buzz(self):
+        print("Testing Buzz ...")
+        self.assertEqual("buzz", fizz_buzz.fizz_buzz(5).split()[-1].lower())
+
+    def test_ping(self):
+        print("Testing Ping ...")
+        self.assertEqual("ping", fizz_buzz.fizz_buzz(7).split()[-1].lower())
+
+    def test_pong(self):
+        print("Testing Pong ...")
+        self.assertEqual("pong", fizz_buzz.fizz_buzz(11).split()[-1].lower())
+
+    def test_all(self):
+        print("Testing 1155 ...")
+        self.assertEqual("fizzbuzzpingpong", fizz_buzz.fizz_buzz(1155).split()[-1].lower())
+
