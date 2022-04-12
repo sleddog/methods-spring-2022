2c85af6aeb65f5820d4068a2edb48b3e288b57f4
W. Ward <woward00@gmail.com>
2022-04-11 19:16:38 -0600

    make fizzbuzz function return string rather than print to increase testability

--- fizzbuzz/python/sierra/fizzbuzz.py
+++ fizzbuzz/python/sierra/fizzbuzz.py
@@ -3,23 +3,24 @@ from typing import Dict
 
 
 def fizzbuzz(n: int):
-
+    return_string: str = ""
     fizz_buzz_dict: Dict[int, str] = {3: "Fizz",
                                       5: "Buzz",
                                       7: "Ping",
                                       11: "Pong"}
-
     for num in range(1, n + 1):
-        print_string: str = ""
+        num_string: str = ""
         for int_key in fizz_buzz_dict.keys():
-            print_string += fizz_buzz_dict[int_key]
-        if print_string == "":
-            print_string = str(num)
-        print(print_string)
+            if num % int_key == 0:
+                num_string += fizz_buzz_dict[int_key]
+        if num_string == "":
+            num_string = str(num)
+        return_string += num_string
+    return return_string
 
 
 def main(number: int):
-    fizzbuzz(number)
+    print(fizzbuzz(number))
 
 
 # Run main using the command line argument
