3ce04e935476d87cbd07bf0e9883a679e7be2145
Mike Gotta <mikejgotta@gmail.com>
2022-04-12 19:14:56 -0600

    added new functionality and unit testing to Logans Fizz Buzz

--- fizzbuzz/python/loganDolechek/fbSolution.py
+++ fizzbuzz/python/loganDolechek/fbSolution.py
@@ -2,9 +2,13 @@
 #Logan Dolechek
 import sys
 
+
 def fizzbuzz(n):
 
     for fb in range(1,n+1):
+        if fb % 1155 == 0:
+          print("FizzBuzzPingPong")
+          continue
         if fb % 15 == 0:
           print("FizzBuzz")
           continue
@@ -13,6 +17,12 @@ def fizzbuzz(n):
            continue
         elif fb % 5 == 0:
            print("Buzz")
+           continue
+        elif fb % 7 == 0:
+           print("Ping")
+           continue
+        elif fb % 11 == 0:
+           print("Pong")
            continue
         print(fb)
 
