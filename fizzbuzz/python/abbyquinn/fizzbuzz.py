d3deb0588787043ad9f073c07cabee72d1424591
Sierra Stephens <sierrastephens21@gmail.com>
2022-04-19 15:04:59 -0600

    Update fizzbuzz.py

--- fizzbuzz/python/abbyquinn/fizzbuzz.py
+++ fizzbuzz/python/abbyquinn/fizzbuzz.py
@@ -23,7 +23,7 @@ def fizzBuzz(val):
             print("PingPong")
         if i%3!=0 and i%5!=0 and i%7!=0 and i%11!=0:
             print(i)
-        if i%3==0 and i%5==0 and i%7==0 and i%11==0:
+        if i%1155==0:
             print("FizzBuzzPingPong")
               
 
