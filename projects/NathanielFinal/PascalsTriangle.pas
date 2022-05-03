75202cb7da96c215c6e8a28e60d20e2d05ac6480
LordThesus <70674757+LordThesus@users.noreply.github.com>
2022-05-03 13:20:47 -0600

    Updated PascalsTriangle.pas
    Writing number of numbers within triangle to console as output.

--- projects/NathanielFinal/PascalsTriangle.pas
+++ projects/NathanielFinal/PascalsTriangle.pas
@@ -1,17 +1,20 @@
 program PascalsTriangle;
 
+uses sysutils;
+
 const
 	max = 100;
 	maxArray = 1000;
 
 var
+	count	: integer;
 	i	: integer;
 	j	: integer;
 	firstArray	: array[1..max+1] of integer;
 	secondArray	: array[1..max+1] of integer;
 
 begin
-	for i := 1 to max+1 do
+	for i := 1 to (max+1) do
 	begin
 		firstArray[i] := 0;
 		secondArray[i] := 0;
@@ -21,13 +24,15 @@ begin
 	firstArray[2] := 1;
 	firstArray[3] := 2;
 
-	for i := 1 to max+1 do
+	for i := 1 to max do
 	begin
 		secondArray[1] := 0;
 		for j := 2 to (maxArray+1) do
 			secondArray[j] := firstArray[j-1]+firstArray[j];
 
 		for j := 1 to (maxArray+1) do
-			firstArray[i] := secondArray[i];
+			firstArray[j] := secondArray[j];
 	end;
+
+	writeln('Total numbers inside triangle :' + floattostr(max/2*(max+1)));
 end.
